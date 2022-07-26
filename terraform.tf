57172de0b98eb28a20ddf195f6d5a53c84fb19dd
STDineshKumar <49930552+STDineshKumar@users.noreply.github.com>
2022-07-26 14:03:49 +0530

    updated the terraform file

--- terraform.tf
+++ terraform.tf
@@ -1,3 +1,26 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+ backend "s3" {
+    bucket = "test-bucket-tilldate"
+   // key  = "path/to/my/key"
+    region = "ap-south-1"
+    
+  }
+}
+
+
+provider "aws" {
+  region = "ap-south-1"
+  access_key = "AKIAQUV6T2NJGLZ2Q5IP"
+  secret_key = "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"
+}
+
+
 resource "aws_instance" "my_instance" {
     //count = 2
     ami           = "ami-08df646e18b182346"
