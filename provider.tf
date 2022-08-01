81e4efd11a7c501ea0de9dac3e518382d8845338
STDineshKumar <49930552+STDineshKumar@users.noreply.github.com>
2022-08-01 19:35:05 +0530

    changed the provider

--- provider.tf
+++ provider.tf
@@ -2,12 +2,19 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.0"
+      version = "4.5.0"
     }
   }
+ backend "s3" {
+    bucket = "test-bucket-tilldate"
+   // key  = "path/to/my/key"
+    region = "ap-south-1"
+    
+  }
 }
+
 provider "aws" {
   region = "ap-south-1"
-  access_key = "AKIAQUV6T2NJGYB5KWFF"
-  secret_key = "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"
+  access_key = "AKIAQUV6T2NJBSIYOBBA"
+  secret_key = "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"
 }
