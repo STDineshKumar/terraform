2f6f93db89103f6942e3aa15108ef147798238fd
STDineshKumar <49930552+STDineshKumar@users.noreply.github.com>
2022-08-01 19:34:06 +0530

    updated the terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -1,38 +1,12 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"
-    }
-  }
- backend "s3" {
-    bucket = "test-bucket-tilldate"
-   // key  = "path/to/my/key"
-    region = "ap-south-1"
-    
-  }
-}
-
-
-provider "aws" {
-  region = "ap-south-1"
-  access_key = "AKIAQUV6T2NJGLZ2Q5IP"
-  secret_key = "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"
-}
-
-
-resource "aws_instance" "my_instance" {
-    //count = 2
-    ami           = "ami-08df646e18b182346"
-    instance_type = "t2.micro"
-    availability_zone = var.availability_zone
-    security_groups = [ "Ssh_http_https" ]
-   
-
-
-  tags={
-    //Name = "count"
-    Name = var.tags
-  }
+module "s3" {
+  source = "terraform-aws-modules/s3-bucket/aws"
+  version = "3.3.0"
   
+}
+resource "aws_s3_bucket" "createS3_bucket" {
+  bucket = "test-bucket-tilldate-${var.bucket_name}"
+ // acl = "private"
+  tags = {
+    Name       = "My bucket"
+  }
 }
